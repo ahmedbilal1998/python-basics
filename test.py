3dbdeb76c38e23d7dc37bf91b1b673f5fe10d7b0
ahmedbilal1998 <97380768+ahmedbilal1998@users.noreply.github.com>
2026-03-16 14:40:13 +0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,16 @@
+name = "haleema"
+age = 5
+marks = 50
+print(name)
+print(age)
+print(marks)
+
+a = 380
+b= 380
+sum= a+b 
+print("value of sum is",sum)
+
+length = 15
+width= 30
+area = length* width
+print("the area of rectangle is" ,area)
