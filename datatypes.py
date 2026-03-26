4fdf50625a5cd870adf09664132ab9590bfe141b
ahmedbilal1998 <97380768+ahmedbilal1998@users.noreply.github.com>
2026-03-26 22:14:27 +0500

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,24 @@
+x= 20
+print(type(x))
+y= 589.98
+print(type(y))
+z= 2j
+print(type(z))
+a= ["haleema","faseeha","bilal"]
+print(type(z))
+b= (10,20,30)
+print(type(b))
+c= range(10)
+print(type(c))
+d= bytearray(19)
+print(type(d))
+e= b"wqaqs"
+print(type(e))
+f= None
+print(type(e))
+g= memoryview(bytes(5))
+print(type(g))
+h= {"name" : "John", "age" : 36}
+print(type(h))
+
+
