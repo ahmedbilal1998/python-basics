ae3c8aa283ae1585d412aee27e880da308199244
ahmedbilal1998 <97380768+ahmedbilal1998@users.noreply.github.com>
2026-03-27 12:44:25 +0500

    Add files via upload

---
+++ pratical task.py
@@ -0,0 +1,21 @@
+Name= "Haleema"
+print(Name)
+Age = 4
+print(Age)
+Marks= 899.90
+print(Marks)
+passed= True
+print(passed)
+
+name= input("enter your name")
+n= str(name)
+print(n)
+print(type(n))
+Age= input("enter your age")
+n= int(Age)
+print(n)
+print(type(n))
+salary= input("enter your salary")
+n= float(salary)
+print(n)
+print(type(n))
