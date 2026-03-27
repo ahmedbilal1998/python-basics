aadd01343d8497683025417553ddfc4935aee09a
ahmedbilal1998 <97380768+ahmedbilal1998@users.noreply.github.com>
2026-03-27 12:43:51 +0500

    Add files via upload

---
+++ lab task.py
@@ -0,0 +1,34 @@
+a= 50
+print(a)
+
+product= 1800.20
+print(product)
+b= "my name is ahmed"
+print(b)
+student= True
+print(student)
+
+item1= 1800.00
+item2= 150.0
+item3= 120.0
+print(item1)
+print(item2)
+print(item3)
+tottalbill = item1+item2+item3
+print(tottalbill)
+
+c=24
+
+f= (24*9/5)+32
+print("tempature in clecius", c)
+print("tempature in faraheight",f) 
+
+number= input("enter the number")
+n= float(a)
+print(a)
+print(type(n))
+n= str(a)
+print(n)
+print(type(n))
+
+
