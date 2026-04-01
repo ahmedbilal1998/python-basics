9f0101132f01e9dccafdb52076eadb24e7036f19
ahmedbilal1998 <97380768+ahmedbilal1998@users.noreply.github.com>
2026-04-01 12:50:53 +0500

    Add files via upload

---
+++ string  assiment.py
@@ -0,0 +1,15 @@
+#q1
+text = input("enter the text")
+result = print(text.upper())
+result= print(text.lower())
+#q2
+a = input("enter the text")
+result = print(len(a))
+#q3
+b= input("enter the  text")
+print(b[0])
+print(b[-1])
+#q4
+c = input("enter the text")
+cleartext= c.strip().lower().capitalize()
+print(cleartext)
